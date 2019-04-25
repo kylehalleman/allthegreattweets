1f315c5dbbe81644bee1e9b9ac348c9dd42f39c4
cknapp <cknapp@watermarkinsights.com>
2019-04-25 12:47:10 -0500

    Pull constants out into globalvars file

--- api/py/request.py
+++ api/py/request.py
@@ -3,6 +3,7 @@
 from http.server import BaseHTTPRequestHandler
 import oauth2
 import json
+from . import globalvars
 
 class Handler(BaseHTTPRequestHandler):
 
@@ -14,18 +15,9 @@ class Handler(BaseHTTPRequestHandler):
         return
 
 
-API_BASE_URL = 'https://api.twitter.com/1.1'
-
-CONSUMER_KEY = 'fqYAlU6lRpoFvWiOrTaSgRmHz'
-CONSUMER_SECRET = 'sdNJzBDMaRGQ1UAcpmJYeFQLygCyvxeTQpEnjJ5xiLFOvvAm97'
-
-ACCESS_KEY = '497370998-0bvV05BIwtWaGwHPAcJKorp0nvcYzTa9RLaFOhbV'
-ACCESS_SECRET = '8GsBrTgwniofjWUvWpifiOevcYsJg3pYul8JziXgTn3H6'
-
-
-def oauth_req(url_suffix, key=ACCESS_KEY, secret=ACCESS_SECRET, http_method='GET', post_body='', http_headers=None):
-    url = API_BASE_URL + '/' + url_suffix
-    consumer = oauth2.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+def oauth_req(url_suffix, key=globalvars.ACCESS_KEY, secret=globalvars.ACCESS_SECRET, http_method='GET', post_body='', http_headers=None):
+    url = globalvars.API_BASE_URL + '/' + url_suffix
+    consumer = oauth2.Consumer(key=globalvars.CONSUMER_KEY, secret=globalvars.CONSUMER_SECRET)
     token = oauth2.Token(key=key, secret=secret)
     client = oauth2.Client(consumer, token)
     resp, content = client.request(url, method=http_method, body=post_body.encode(), headers=http_headers)
