0211a8474d8b15b8bbb9f1d0e460a296e9c52437
Derek Fetting <dfetting@watermarkinsights.com>
2019-04-25 16:00:25 -0500

    fixed issue with query param

--- api/py/index.py
+++ api/py/index.py
@@ -28,21 +28,29 @@ class Handler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
 
+        output_str = ''
         query_string = self.path
-        params = parse_qs(query_string[2:])
-        username = params['name']
-        self.wfile.write('queried: ' + username)
-        output = self.get_followed_tweets(self.get_followed_usernames(username))
-        json_obj = {}
-        following_list = []
-        for username in output:
-            user_data = {'username': username, 'tweets': output[username]}
-            following_list.append(user_data)
-        json_obj['following'] = following_list
-        self.wfile.write(json.dumps(json_obj, indent=2).encode())
+        if len(query_string) < 2:
+            output_str = 'No query param supplied.'
+        else:
+            params = parse_qs(query_string[2:])
+            if 'name' not in params:
+                output_str = 'Expected "name" query param.'
+            else:
+                username = params['name']
+                self.wfile.write('queried: ' + username)
+                output = self.get_followed_tweets(self.get_followed_usernames(username))
+                json_obj = {}
+                following_list = []
+                for username in output:
+                    user_data = {'username': username, 'tweets': output[username]}
+                    following_list.append(user_data)
+                json_obj['following'] = following_list
+                output_str = json.dumps(json_obj, indent=2)
+        self.wfile.write(output_str.encode())
         return
 
-    def oauth_req(url_suffix, key=ACCESS_KEY, secret=ACCESS_SECRET, http_method='GET', post_body='', http_headers=None):
+    def oauth_req(self, url_suffix, key=ACCESS_KEY, secret=ACCESS_SECRET, http_method='GET', post_body='', http_headers=None):
         url = API_BASE_URL + '/' + url_suffix
         consumer = oauth2.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
         token = oauth2.Token(key=key, secret=secret)
@@ -51,15 +59,12 @@ class Handler(BaseHTTPRequestHandler):
         # print('RESP: ' + str(resp))
         return json.loads(content.decode())
 
-
-    # obtain list of followed accounts
-    # return up to 900 screen names in list
     def get_followed_usernames(self, username):
+        """obtain list of followed accounts. return up to 900 screen names in list"""
         follows_json = self.oauth_req('friends/list.json?screen_name=' + username)
         user_list = follows_json['users']
         usernames = [x['screen_name'] for x in user_list]
         return usernames
-
 
     def get_followed_tweets(self, usernames):
         """for each item in list of usernames, obtain up to 200 tweets"""
@@ -68,7 +73,7 @@ class Handler(BaseHTTPRequestHandler):
         for username in usernames[:max_index]:
             user_tweets = 0
             tweets_list = self.oauth_req('statuses/user_timeline.json?screen_name=' + str(username) + EXCLUDE_REPLIES + INCLUDE_RTS + MAX_TWEETS)
-    # filter tweets by timestamp
+            # filter tweets by timestamp
             current_time = datetime.datetime.now(datetime.timezone.utc)
             date_format = '%a %b %d %H:%M:%S %z %Y'
             for tweet in tweets_list:
