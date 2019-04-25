080e7dac7de85d04ff10b0560ba4233e8e1905fc
Kyle Halleman <khalleman@watermarkinsights.com>
2019-04-25 10:35:33 -0500

    basic search form to index

--- www/pages/index.js
+++ www/pages/index.js
@@ -1,10 +1,19 @@
 import React from "react";
-import Link from "next/link";
+import { withRouter } from "next/router";
 
-const Home = () => (
-  <div>
-    <Link href="/user">Submit! (but doesn't actually do anything really)</Link>
-  </div>
-);
+function Home({ router }) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    router.push(`/user?name=${e.target.elements.username.value}`);
+  }
 
-export default Home;
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="username" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+export default withRouter(Home);
