4ef24f88683b4d8e7b25dbf16e56d7831ba11e8d
Derek Fetting <dfetting@watermarkinsights.com>
2019-04-25 11:40:26 -0500

    updated method name

--- api/py/index.py
+++ api/py/index.py
@@ -5,7 +5,7 @@ from http.server import BaseHTTPRequestHandler
 
 class Handler(BaseHTTPRequestHandler):
 
-    def do_get(self):
+    def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
