7c6e793cefc41c7676ed7ea102b605ac2fa3cbec
Kyle Halleman <khalleman@watermarkinsights.com>
2019-04-25 11:23:52 -0500

    show user name

--- api/get-follow-count.js
+++ api/get-follow-count.js
@@ -1,4 +1,6 @@
 module.exports = (req, res) => {
-	// do something
-	// https://zeit.co/docs/v2/deployments/concepts/lambdas/
-}
+  // do something
+  // https://zeit.co/docs/v2/deployments/concepts/lambdas/
+  console.log("you called the api");
+  res.send("you called the api");
+};
--- www/pages/user.js
+++ www/pages/user.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import fetch from "isomorphic-unfetch";
 
-const User = () => <div>User!</div>;
+function User({ url }) {
+  useEffect(() => {
+    fetch(
+      `${window.location.origin}/api/get-follow-count?user=${url.query.name}`
+    );
+  }, [url.query.name]);
+  return <>User: {url.query.name}</>;
+}
+
+User.getInitialProps = async ({ req, query }) => {
+  // console.log(req.headers);
+  // const user = await fetch(
+  //   `https://${req.headers.host}/api/get-follow-count?user=${query.name}`
+  // );
+  // console.log(user);
+  // return {
+  //   user
+  // };
+  return {};
+};
 
 export default User;
