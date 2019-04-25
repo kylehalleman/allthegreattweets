1efe1f70e82eaf703e815d3a495626e8c0cf2e71
Kyle Halleman <khalleman@watermarkinsights.com>
2019-04-25 14:06:02 -0500

    endpoint mock, user page

--- www/pages/user.js
+++ www/pages/user.js
@@ -1,23 +1,81 @@
-import React, { useEffect } from "react";
+import React, { useReducer } from "react";
 import fetch from "isomorphic-unfetch";
+import json from "../static/following.json";
+import "./users.styl";
 
-function User({ url }) {
-  useEffect(() => {
-    fetch(`${window.location.origin}/api/py/request?user=${url.query.name}`);
-  }, [url.query.name]);
-  return <>User: {url.query.name}</>;
+function reducer(state, action) {
+  switch (action.type) {
+    case "ascending":
+      return {
+        sort: "ascending",
+        list: sortAscending(state.list)
+      };
+    case "descending":
+      return {
+        sort: "descending",
+        list: sortDescending(state.list)
+      };
+    default:
+      return state;
+  }
+}
+
+function sortAscending(list) {
+  return list.slice(0).sort((a, b) => b.tweets - a.tweets);
+}
+
+function sortDescending(list) {
+  return list.slice(0).sort((a, b) => a.tweets - b.tweets);
+}
+
+function User({ url, user }) {
+  const [{ list, sort }, dispatch] = useReducer(reducer, {
+    sort: "descending",
+    list: sortAscending(json.following)
+  });
+
+  function sortColumn() {
+    dispatch({
+      type: sort === "descending" ? "ascending" : "descending"
+    });
+  }
+
+  return (
+    <>
+      <h1>Who {url.query.name} follows</h1>
+      <table className="users-table">
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th aria-sort={sort}>
+              <button onClick={sortColumn}>
+                Tweet Count <span className="sr-only">click to sort</span>
+              </button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {list.map(({ username, tweets }) => {
+            return (
+              <tr key={username}>
+                <td>{username}</td>
+                <td>{tweets}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 User.getInitialProps = async ({ req, query }) => {
-  // console.log(req.headers);
-  // const user = await fetch(
-  //   `https://${req.headers.host}/api/get-follow-count?user=${query.name}`
-  // );
-  // console.log(user);
-  // return {
-  //   user
-  // };
   return {};
+  const url = req
+    ? `https://${req.headers.host}/api/py/user.py?name=${query.name}`
+    : `${window.location.origin}/api/py/user.py?name=${query.name}`;
+  const user = await fetch(url);
+  return { user };
 };
 
 export default User;
