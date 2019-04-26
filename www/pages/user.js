ce43d4b461c8361cd4486b340e83724886fb2be1
Kyle Halleman <khalleman@watermarkinsights.com>
2019-04-26 14:23:02 -0500

    deploy on deploy branch only? node api

--- www/pages/user.js
+++ www/pages/user.js
@@ -94,8 +94,8 @@ function User({ url, user }) {
 
 User.getInitialProps = async ({ req, query }) => {
   const url = req
-    ? `http://${req.headers.host}/api/py?name=${query.name}`
-    : `${window.location.origin}/api/py?name=${query.name}`;
+    ? `http://${req.headers.host}/api/node?name=${query.name}`
+    : `${window.location.origin}/api/node?name=${query.name}`;
   const user = await fetch(url);
   const json = await user.json();
   return { user: json.following };
