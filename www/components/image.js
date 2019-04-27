8ef74186111ffa7ab6d29746c48c2f27a6eee424
Kyle Halleman <khalleman@watermarkinsights.com>
2019-04-26 22:35:32 -0500

    remove canvas pixelation as it is not needed?

--- www/components/image.js
+++ www/components/image.js
@@ -1,42 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 
 export default function Canvas({ src, username }) {
-  const [hasJs, setJsState] = useState(false);
-  const ref = useRef(null);
   const mini = src.replace('_normal', '_mini');
 
-  useEffect(() => {
-    var ctx = ref.current.getContext('2d');
-    // load image
-    var image = new Image();
-    image.onload = function() {
-      // draw the image into the canvas
-      setJsState(true);
-      ctx.drawImage(image, 0, 0);
-    };
-    image.src = mini;
-  }, [mini]);
-
   return (
-    <>
-      <img
-        src={mini}
-        alt={`Profile pic for ${username}`}
-        style={{
-          display: hasJs ? 'none' : 'block',
-          visibility: hasJs ? 'hidden' : 'visible'
-        }}
-      />
-      <canvas
-        ref={ref}
-        className="canvas"
-        width="24"
-        height="24"
-        style={{
-          display: hasJs ? 'block' : 'none',
-          visibility: hasJs ? 'visible' : 'hidden'
-        }}
-      />
-    </>
+    <img src={mini} alt={`Profile pic for ${username}`} className="canvas" />
   );
 }
